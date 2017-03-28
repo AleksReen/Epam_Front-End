a4469b7b68b1043a080adb280179235b053dcd21
AleksReen <aleksandr.reentovich@tut.by>
2017-03-28 16:37:07 +0300

    DragAndDrop

---
+++ DragAndDrop/DragAndDrop.js
@@ -0,0 +1,19 @@
+﻿
+var div = document.querySelectorAll(".img");
+for (var i = 0; i < div.length; i++) {
+    div[i].id = "drag"+i;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
